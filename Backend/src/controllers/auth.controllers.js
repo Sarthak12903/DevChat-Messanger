b3c0f77e3fe3732a67614150bf158ea74b86caf6
Nitesh <niteshranjankar5@gmail.com>
2025-06-22 22:46:16 +0530

    Add checkAuth controller to return authenticated user information

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -123,3 +123,11 @@ export const updateProfile = async (req, res) => {
   }
 };
 
+export const checkAuth = async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log(("Error in checkAuth controller ", error.message));
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
