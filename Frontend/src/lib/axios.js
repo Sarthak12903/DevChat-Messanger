33c31274ee86c984474ea427a3c1abae88484c0c
Nitesh <niteshranjankar5@gmail.com>
2025-06-22 16:44:34 +0530

    Fixed!

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "/api",
+      : "https://dcm-3hk4.onrender.com/api",
   withCredentials: true,
 });
